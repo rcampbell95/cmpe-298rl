9532bf0f1c07a0f5feea2ec47dc8603bb736400f
Muslim Razi <muslimrazi@Muslims-MacBook-Pro.local>
2020-02-15 23:25:29 -0800

    hw2-full

--- 2/agent.py
+++ 2/agent.py
@@ -1,6 +1,11 @@
+import random
 
 class Agent():
     def __init__(self):
-        pass
+        self.total_reward = 0.0
 
-
+    def step(self, env):
+        self.current_obs = env.get_observation()
+        actions = env.get_actions()
+        reward = env.action(random.choice(actions))
+        self.total_reward += reward
--- 2/test_agent_environment.py
+++ 2/test_agent_environment.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     env = Environment()
     test_agent = Agent()
 
+    steps = 0
     while not env.is_done():
-        test_agent.step()
+        print ("Current step: {}".format(steps))
+        test_agent.step(env)
+        steps += 1
 
     print("Total rewards: {}".format(test_agent.total_reward))
-
-
-
